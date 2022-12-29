cb57be4737ee7d4aaef50164562b3eca7c8c72b2
Ewxun <93865977+Ewxun@users.noreply.github.com>
2022-12-29 15:15:49 +0800

    New example

--- examples/bot.py
+++ examples/bot.py
@@ -1,14 +1,16 @@
 import os
 
-from discord import Intents
+import discord
 from discord.ext import commands
-from discord.ext.commands.context import Context
 from dotenv import load_dotenv
 
 load_dotenv(".env")
 
-intents = Intents.default()
+intents = discord.Intents.default()
 intents.message_content = True
+
+# paste guild ID here
+GUILD_ID = 12345678
 
 bot = commands.Bot(command_prefix="?", intents=intents)
 
@@ -23,30 +25,22 @@ bot.spotify_credentials = {
     "client_secret": "CLIENT_SECRET_HERE",
 }
 
+# If you want the slash commands to be prefixed with "music" set to True
+bot.music_prefixed = True
+
 
 @bot.event
 async def on_ready():
     print(f"{bot.user} is ready")
+    MY_GUILD = discord.Object(id=GUILD_ID)
+    bot.tree.copy_global_to(guild=MY_GUILD)
+    await bot.tree.sync(guild=MY_GUILD)
 
 
-@bot.command()
-async def rickroll(ctx: Context):
-    """Never gonna give you up"""
-    await ctx.invoke(
-        bot.get_command("play"), query="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-    )
-    await ctx.send("Never gonna give you up")
+@bot.tree.command(name="ping", description="Pong")
+async def ping_command(interaction: discord.Interaction):
+    await interaction.response.send_message(embed=discord.Embed(title="Pong!"))
 
 
-@bot.command()
-async def kids(ctx: Context):
-    """Test playlist playing"""
-    await ctx.invoke(
-        bot.get_command("play"),
-        query="https://www.youtube.com/playlist?list=PL2MHGlY_k-FG_Wc83QiOWj1-P5aXv-Tsm",
-    )
-    await ctx.send("Some kids song added for you")
-
-
-bot.load_extension("dismusic")
+bot.load_extension("dismusic-slash")
 bot.run(os.getenv("TOKEN"))
